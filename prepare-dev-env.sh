0c3ba4704665b8035236723b16fa1bf1b4175575
chrischen <chrischen@ignity.xyz>
2024-04-25 13:53:07 +0800

    configure dev environment

--- prepare-dev-env.sh
+++ prepare-dev-env.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -u -o pipefail
 # install conan via pipx or pip
 if [ -x "$(command -v pipx)" ]; then
@@ -13,4 +13,7 @@ if ! conan profile path gnu20 ; then
     PROFILE_PATH=$(conan profile path gnu20)
     sed -i 's/compiler\.cppstd=[^ ]*/compiler.cppstd=gnu20/g' $PROFILE_PATH
 fi
-conan install . -pr gnu20 --output-folder=build --build=missing
+conan install . --output-folder=build --build=missing \
+    --profile:host=gnu20 --profile:build=gnu20
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
