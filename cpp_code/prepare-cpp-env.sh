5f500f32d99cd4a014bde8e72bfeba9d8b7adb33
chrischen <chrischen@ignity.xyz>
2024-12-18 19:26:18 +0800

    reorganize project structure

--- cpp_code/prepare-cpp-env.sh
+++ cpp_code/prepare-cpp-env.sh
@@ -13,7 +13,7 @@ if ! conan profile path gnu20 ; then
     PROFILE_PATH=$(conan profile path gnu20)
     sed -i 's/compiler\.cppstd=[^ ]*/compiler.cppstd=gnu20/g' "$PROFILE_PATH"
 fi
-conan install ./cpp_code/ --output-folder=cppbuild --build=missing \
+conan install ./cpp_code/ --output-folder=build --build=missing \
     --profile:host=gnu20 --profile:build=gnu20
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
